a72abb4d4df4213e666efe4375881d6dc0bd69c0
naglit <docono.ne.ip@gmail.com>
2021-01-29 11:49:26 +0900

    only main prcess flow

--- src/TableDefinitionBrowser/Areas/TableDefinition/Controllers/HomeController.cs
+++ src/TableDefinitionBrowser/Areas/TableDefinition/Controllers/HomeController.cs
@@ -1,10 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using TableDefinitionBrowser.DataAccess.Data;
 using TableDefinitionBrowser.Models;
 
 namespace TableDefinitionBrowser.Controllers
@@ -14,15 +16,17 @@ namespace TableDefinitionBrowser.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            return View(await _context.TableDefinition.ToListAsync());
         }
 
         public IActionResult Privacy()
